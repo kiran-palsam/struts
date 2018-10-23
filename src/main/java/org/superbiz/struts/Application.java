fa851ed5da563ea9fc7d1a38608125e5936cb813
gaijfox <jfox@gaig.com>
2018-10-23 08:44:58 -0600

    struts conversion

--- src/main/java/org/superbiz/struts/Application.java
+++ src/main/java/org/superbiz/struts/Application.java
@@ -1,4 +1,39 @@
 package org.superbiz.struts;
 
+import com.opensymphony.module.sitemesh.filter.PageFilter;
+import org.apache.struts2.dispatcher.ActionContextCleanUp;
+import org.apache.struts2.dispatcher.FilterDispatcher;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Collections;
+
+@SpringBootApplication
 public class Application {
+
+
+    @Bean
+    public FilterRegistrationBean filterDispatcher() {
+        FilterDispatcher filterDispatcher = new FilterDispatcher();
+        return new FilterRegistrationBean(filterDispatcher);
+    }
+
+    @Bean
+    public FilterRegistrationBean cleanup() {
+        ActionContextCleanUp actionContextCleanup = new ActionContextCleanUp();
+        return new FilterRegistrationBean(actionContextCleanup);
+    }
+
+    @Bean
+    public FilterRegistrationBean sitemesh() {
+        PageFilter pageFilter = new PageFilter();
+        return new FilterRegistrationBean(pageFilter);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
+
--- src/main/java/org/superbiz/struts/User.java
+++ src/main/java/org/superbiz/struts/User.java
@@ -17,6 +17,8 @@
 */
 package org.superbiz.struts;
 
+import org.springframework.stereotype.Component;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
